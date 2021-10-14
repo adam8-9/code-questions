2701b69e76c2027b032825c7bfbb49921cdc5dfd
Adam <31065348+adam8-9@users.noreply.github.com>
2021-10-14 14:44:30 +0100

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,42 @@
+function filter(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]%2 === 0){
+        result.push(array[i])
+        }
+    }
+    return result
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+console.log(array([1, 2, 3, 4]));
