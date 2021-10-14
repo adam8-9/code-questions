ca19e2e683075099f1235aabfb6382f023f8ea3a
Adam <31065348+adam8-9@users.noreply.github.com>
2021-10-14 14:50:22 +0100

    Create code2.js

---
+++ code2.js
@@ -0,0 +1,40 @@
+function transform(arg1) {
+    let result = [];
+    for (let i = arg1.length-1; i > 0; i--) {
+        result.push(arg1[i])
+    }
+    return result
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+console.log(transform("racecar"));
