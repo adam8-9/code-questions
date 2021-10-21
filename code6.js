ee0f8982cf1725ae3beb86f5a2e69c29fbf8ff2b
Adam <31065348+adam8-9@users.noreply.github.com>
2021-10-21 16:54:47 +0100

    Create code6.js

---
+++ code6.js
@@ -0,0 +1,39 @@
+function match(arg1,arg2){
+let result = false
+let val1 = arg1.length
+let val2 = arg2.length
+if(val1 % 2 === 0 && val2 % ===0){
+  result = true
+}
+return result
+
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+console.log(match([890,45,22,11],[82,2,3,4,]))
