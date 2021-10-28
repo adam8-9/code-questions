717d0320ef732af4d7e0a4fa6e3cfb189c0252df
Adam <31065348+adam8-9@users.noreply.github.com>
2021-10-28 16:54:40 +0100

    Create code7.js

---
+++ code7.js
@@ -0,0 +1,32 @@
+function match(arg1,arg2){
+let output = ''
+let first = typeof(arg1)
+let second = typeof(arg2)
+if (first === second){
+    output = first
+}
+  return output
+
+
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+console.log(match('apple','orange'))
