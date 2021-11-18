cd5776a1405b4f82d74746703f7fca1ab76c5836
Adam <31065348+adam8-9@users.noreply.github.com>
2021-11-18 16:11:24 +0000

    Create code12.js

---
+++ code12.js
@@ -0,0 +1,32 @@
+function func(x){
+  let y = x.splice(0)
+  let j = x.slice()
+
+  return y === j
+  
+}
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+\\
+console.log(func([1,2,3,4]))
